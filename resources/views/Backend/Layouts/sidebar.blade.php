049dfd7d5de9df8954179e6a482bd4974fd16d9b
sumon <18103159@iubat.edu>
2024-01-01 12:03:40 +0600

    myAdminPanel

---
+++ resources/views/Backend/Layouts/sidebar.blade.php
@@ -0,0 +1,21 @@
+<div class="sidebar sidebar-hide-to-small sidebar-shrink sidebar-gestures">
+    <div class="nano">
+        <div class="nano-content">
+            <ul>
+                <div class="logo">
+                    <a href="{{ route('mstr') }}">
+                        <!-- <img src="assets/images/logo.png" alt="" /> --><span>My Admin Panel</span>
+                    </a>
+                </div>
+                <li class="active">
+                    <a class="sidebar-sub-toggle"><i class="ti-home"></i> Dashboard
+                        <span class="sidebar-collapse-icon ti-angle-down"></span>
+                    </a>
+                    <ul>
+                        <li><a href="{{ route('mstr') }}">Dashboard </a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
